42eeb3e6e7c153d14f362499d6e189cada09aec1
Jingtian Li <jingtianli.animation@gmail.com>
2022-08-24 21:52:14 -0500

    fixed III, Jr issue, seperated first last name

--- main.py
+++ main.py
@@ -5,28 +5,37 @@ df = pandas.read_csv("../Assets/webInq.csv")
 
 outDf = pandas.DataFrame()
 
-names = []
+firstNames = []
+lastNames = []
 emails = []
 
 for item in df.Body:
     allWords = item.split(' ')
-    name = ""
+    name = []
     for word in allWords:
         if "has" in word:
             if "\u2009" in word:
-                name += word.split("\u2009")[0]
+                name.append(word.split("\u2009")[0])
             break
-        name += word + " "
+        name.append(word)
 
     email = ""
     for i in range(0, len(allWords)):
         if "Email:" in allWords[i]:
             email = allWords[i+1][:-7]
 
-    names.append(name)
+    firstNames.append(name[0])
+
+    name.reverse()
+    for i in name:
+        if i != "" and i != "III" and i != "Jr" and i != "II" and i != "I":
+            lastNames.append(i)
+            break
+
     emails.append(email)
 
-outDf["names"] = names
+outDf["first names"] = firstNames
+outDf["last names"] = lastNames
 outDf["emails"] = emails
 
 outDf.to_csv("../Assets/nameEmail.csv", index=False)
