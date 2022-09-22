f4e219d0fbd90639919ef39d99568c3acaaae6bf
Arbint <cg.jingtianli@gmail.com>
2022-09-22 09:24:30 -0500

    added git lfs track helper with ui

--- Git_FindAndTrack_File_With_LFS.py
+++ Git_FindAndTrack_File_With_LFS.py
@@ -2,14 +2,14 @@ import os
 import subprocess
 import tkinter.filedialog
 from tkinter import *
-from os import listdir
 from os.path import join, getsize
 
 winBg = "#080808"
 winFg = "#bfbfbf"
 tfBg = "#222222"
 fnt = "none 12 bold"
-
+winHeight = 300
+winWidth = 300
 def getAllFilesInDir(dir, sizeFilter=100):
     allFiles = []
     for path, currentDir, files in os.walk(dir):
@@ -20,7 +20,8 @@ def getAllFilesInDir(dir, sizeFilter=100):
                 fullPath = join(path, file)
                 relativePath = fullPath.replace(dir+"\\", "")
                 relativePath = relativePath.replace("\\", "/")
-                allFiles.append(relativePath)
+                if ".git" not in relativePath:
+                    allFiles.append(relativePath)
     return allFiles
 
 def browseDirButtonCmd(entry):
@@ -31,29 +32,45 @@ def browseDirButtonCmd(entry):
 def trackFiles(sizeEntryField, DirEntryField, outPut):
     size = int(sizeEntryField.get())
     dir = DirEntryField.get()
-    allFiles = getAllFilesInDir(dir)
+    allFiles = getAllFilesInDir(dir, size)
+    cmd = ["git", "lfs", "track"]
     for file in allFiles:
+        cmd.append(file)
         outPut.insert(END, file + "\n")
-        subprocess.Popen(["git", "lfs", "track", file], cwd=dir)
+
+    subprocess.Popen(cmd, cwd=dir)
 
 window = Tk()
 window.title("Git Track LFS")
-window.config(background="black", height=200, width=200)
+window.config(background=winBg, height=winHeight, width=winWidth)
+window.resizable(False, True)
 
-windowLabel = PhotoImage(file="assets/LFS_Tracker_Label.gif")
-Label(window, image=windowLabel, bg="black").grid(row=0, column=0, sticky=E)
+canvas = Canvas(width=winWidth, height=200, bg=winBg, highlightthickness=0)
+uiw_img = PhotoImage(file="assets/LFS_Tracker_Label.gif")
+canvas.create_image(winHeight/2, winWidth/2, image=uiw_img)
+canvas.grid(row=0,column=0)
+
+#Label(window, image=windowLabel, bg="black").grid(row=0, column=0, sticky=E)
 
 Label(window, text="Please select the directory:", bg=winBg, fg=winFg, font=fnt).grid(row=1, column=0, sticky=W)
 DirEntry = Entry(window, width=50, bg=tfBg, fg=winFg, font=fnt)
 DirEntry.grid(row=2, column=0, sticky=W)
 Button(window, text="...", command=lambda: browseDirButtonCmd(DirEntry), bg=winBg, fg=winFg, font=fnt, height=1).grid(
     row=2, column=1, sticky=W)
+
 Label(window, text="Min Size: ", bg=winBg, fg=winFg, font=fnt).grid(row=3, column=0, sticky=W)
-SizeEntry = Entry(window, width=50, bg=tfBg, fg=winFg, font=fnt)
+SizeEntry = Entry(window, width=53, bg=tfBg, fg=winFg, font=fnt)
 SizeEntry.insert(0, "100")
-SizeEntry.grid(row=4, column=0, sticky=W)
-Output = Text(window, width=50, height=50, wrap=WORD, background=tfBg,fg=winFg,font=fnt)
+SizeEntry.grid(row=4, column=0, columnspan=2)
+
+OutScrollBar = Scrollbar(orient="vertical")
+OutScrollBar.grid(row=6, column=2)
+
+Output = Text(window, width=53, height=50,  wrap=WORD, background=tfBg, fg=winFg, font=fnt)
 Output.grid(row=6, column=0, sticky=W)
+Output['yscrollcommand'] = OutScrollBar.set
+
+
 Button(window, text="Track", command=lambda: trackFiles(SizeEntry, DirEntry, Output), width=30, bg=winBg, fg=winFg,
        font=fnt, height=1).grid(row=5, column=0, sticky=W)
 window.mainloop()
