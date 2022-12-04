ba366cc3fbfc91c41bfcb83e40ed05e220038b68
Arbint <cg.jingtianli@gmail.com>
2022-12-04 11:13:31 -0600

    web inq console app done

--- EmailReplier.py
+++ EmailReplier.py
@@ -2,15 +2,9 @@
 import win32com.client as win32
 import re
 
-def GetInboxMsg(**kwargs):
-    subject = kwargs.get('subject')
-    onlyUnRead = kwargs.get('onlyUnRead')
-
+def GetInboxMsg(subject, onlyUnRead):
     outlook = win32.Dispatch('outlook.application')
     mapi = outlook.GetNamespace('MAPI')
-
-    for account in mapi.Accounts:
-        print(account.DeliveryStore.DisplayName)
 
     messages = mapi.Folders(1).Folders("Inbox").Items
     messages = messages.Restrict(f'[Subject] = {subject}')
@@ -18,13 +12,13 @@ def GetInboxMsg(**kwargs):
     return messages
 
 
-def GetIboxMsgBodyAsList(**kwargs):
-    subject = kwargs.get('subject')
-    onlyUnRead = kwargs.get('onlyUnRead')
+def GetIboxMsgBodyAsList(subject, onlyUnRead, MarkRead):
     EmailMsgs = GetInboxMsg(subject=subject, onlyUnRead=onlyUnRead)
     allEmails = []
     for msg in list(EmailMsgs):
         allEmails.append(msg.body)
+        msg.UnRead = not MarkRead
+
     return allEmails
 
 
@@ -43,8 +37,44 @@ def GetContactFromMsg(msg):
 
     return fullName, email, phone
 
-EmailMsgs = GetIboxMsgBodyAsList(subject='New Web Inquiry Received for Master of Game Development', onlyUnRead=True)
+def ComposeEmail(name):
+    msg = f"""Dear {name},
 
-for Emailmsg in EmailMsgs:
-    name, email, phone = GetContactFromMsg(Emailmsg)
-    print(f"found student {name} with email: {email}, and phone: {phone}")
+This is Professor Li, director of the Master of Game Development program.
+We have received a web inquiry from you, and thank you for your interest in the program.
+If you have any questions about the program, please do not hesitate to reply to this email directly, and I am more than happy to answer them.
+
+Thank you!
+JT
+"""
+    return msg
+
+def SendOutlookEmailTo(address, subject, msg):
+    outlook = win32.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+    mail.Subject = subject
+    mail.To = address
+    mail.Body = msg
+    mail.Send()
+
+def CheckAndSendWebInqReplyMsg():
+    EmailMsgs = GetIboxMsgBodyAsList(subject='New Web Inquiry Received for Master of Game Development', onlyUnRead=True, MarkRead=True)
+    msgCount = len(EmailMsgs)
+    if msgCount == 0:
+        print("there are not messages found")
+        return
+
+    print("this is how the msg will look like: \n")
+    testName, textEmail, testPhone = GetContactFromMsg(EmailMsgs[0])
+    print(ComposeEmail(testName.split(' ')[0]))
+
+    confirm = input(f"{print(msgCount)} messages will be send out, confirm msg?(y/n)\n")
+
+    if confirm == 'y':
+        for Emailmsg in EmailMsgs:
+            name, email, phone = GetContactFromMsg(Emailmsg)
+            msg = ComposeEmail(name.split(' ')[0])
+
+            SendOutlookEmailTo(email, "Thank you for your interest in MGD!", msg)
+
+CheckAndSendWebInqReplyMsg()
